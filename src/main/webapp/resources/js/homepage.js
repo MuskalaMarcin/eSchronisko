38d0aa6f31f3cf46ac6cb33d5de8372cf8341364
Marcin Muskala <marcin.muskala@gmail.com>
2016-11-09 22:17:39 +0100

    solved layout issues

--- src/main/webapp/resources/js/homepage.js
+++ src/main/webapp/resources/js/homepage.js
@@ -1,17 +1,11 @@
 $(document).ready(function () {
     _setLogo()
-    _fillWholePage()
-    $(window).resize(_fillWholePage);
 })
 
 function _setLogo() {
     var logoBackgrounds = ['logoDog.jpg', 'logoCat.jpg']
     var randomBackground = Math.floor(Math.random() * logoBackgrounds.length);
-    $('.logo').css('background-image', 'url(../../resources/images/logo/' + logoBackgrounds[randomBackground] + ')');
-}
-
-function _fillWholePage(){
-    console.log("resize");
+    $('#logo').css('background-image', 'url(../../resources/images/logo/' + logoBackgrounds[randomBackground] + ')');
 }
 
 function scrollPageToTop() {
