099024807884567354c007db528813ea099d05f0
Marcin Muskala <marcin.muskala@gmail.com>
2016-11-09 14:59:32 +0100

    frontend work in progress

--- src/main/java/com/eschronisko/config/SecurityConfig.java
+++ src/main/java/com/eschronisko/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.eschronisko.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -16,4 +17,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+	http.authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated();
+    }
 }
